f4421b5f7a66259e953a7a2bb7a478ddc3d58055
yougren <eugenemunblit@gmail.com>
2017-11-29 18:23:46 -0800

    ajax user info server request stuff

--- client/src/components/loginPopup.vue
+++ client/src/components/loginPopup.vue
@@ -3,10 +3,45 @@
     <div class =  "modalContent">
       <div class = "inputcontainer">
         <h2>Sco Beavs</h2>
-        <input type = "text" name = "username" placeholder="Username"><br>
-        <input type = "text" name = "password" placeholder="Password">
-        <button id = "loginButton">Log In</button>
+        <input v-model = "username" placeholder="Username"><br>
+        <input v-model = "password" placeholder="Password">
+        <button id = "loginButton" v-on:click = "loginRequest">Log In</button>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return {
+        username: "",
+        password: ""
+      }
+    },
+
+    methods : {
+      loginRequest : function(){
+        var userInfo = {
+          username: this.$data.username,
+          password: this.$data.password,
+          token: ""
+        };
+
+        console.log(userInfo);
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', 'localhost:3000/api/user/', userInfo);
+
+        var url = "localhost:3000/?usertoken=69420";
+        var urlParams = new URLSearchParams(window.location.search);
+
+        userInfo.token = urlParams.toString();
+        console.log(userInfo);
+
+        xhr.open('POST', 'localhost:3000/api/user/', userInfo);
+      }
+    }
+  }
+</script>
