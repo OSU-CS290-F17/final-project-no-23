a358b3373c5c21177cef633d3d37e9ab590ddd1b
yougren <eugenemunblit@gmail.com>
2017-11-29 16:29:38 -0800

    login page and popup are vueified now

---
+++ client/src/components/loginPopup.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id = "loginModal" class = "modal">
+    <div class =  "modalContent">
+      <div class = "inputcontainer">
+        <h2>Sco Beavs</h2>
+        <input type = "text" name = "username" placeholder="Username"><br>
+        <input type = "text" name = "password" placeholder="Password">
+        <button id = "loginButton">Log In</button>
+      </div>
+    </div>
+  </div>
+</template>
