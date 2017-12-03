1924d290950b2134f6afd9c05a835661540cc659
yougren <eugenemunblit@gmail.com>
2017-12-02 19:18:05 -0800

    client-side song searching maybe working now

--- client/src/components/searchBar.vue
+++ client/src/components/searchBar.vue
@@ -1,24 +1,54 @@
 <template>
   <div id='searchResults'>
     <input v-model='searchQuery' placeholder='Song Name'>
+    <ol id ='songs'>
+      <li v-for="song in songInfo">
+        <div>
+          <h2>{{song.name}}</h2>
+          <h2>{{song.artist}}</h2>
+          <button id ='addSong' class = 'smallButton'></button>
+        </div>
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
+  const axios = require('axios');
+
+
   export default{
     data(){
       return {
-        searchQuery = '';
+        searchQuery = '',
+        songInfo = {},
+        username = ""
       }
-    }
-
+    },
+    created : function(){
+      this.$data.username = Cookies.get('username');
+    },
     methods:{
       search = function(){
-
+        axios.post(Globals.apiHost + "/search/searchtracks", {
+            username : that.$data.username,
+            query: searchQuery
+        }).then((data) => {  //request to endpoint that will add group to DB
+            console.log("Track searched");
+            for (track in data.tracks.items) {
+              for(artist in track.artists){
+                songInfo.push({
+                  name : track.name,
+                  artist: artists.name,
+                  id: artists.id
+                });
+              }
+            }
+        }).catch((error) => {console.log(error);});
       }
 
       returnResults = function(){
-        
+
       }
     }
   }
